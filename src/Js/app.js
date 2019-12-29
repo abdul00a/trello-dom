fb18c5de320f5f7ddbfdf54a87b7814a907c9bf1
Abdul <abdul.ashraf@mountblue.tech>
2019-12-29 12:17:31 +0530

    checklist added and integrated with trello api

--- src/Js/app.js
+++ src/Js/app.js
@@ -17,32 +17,89 @@ const createcard = (value, id) => {
   const cards = `<li data-id="${id}" data-target="${id}" class="modal-trigger">
   <div class="card small cards">
     ${value}
-    <i class="tiny material-icons close-card">cancel</i>
+    <i class="tiny material-icons close-card" data-type="deleteCard">cancel</i>
   </div>
 </li>`;
   const cardContainer = document.querySelector('#card-container');
   cardContainer.insertAdjacentHTML('beforeend', cards);
-  createModal(value, id);
   return;
 };
 
-const createModal = (value, id) => {
-  const modal = `<div id="${id}" class="modal">
-  <div class="modal-content">
-    <h4>${value}</h4>
-    <p>A bunch of text</p>
-  </div>
-  <div class="modal-footer">
-    <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-  </div>
-</div>`;
-  const modaContainer = document.querySelector('#modal-container');
-  modaContainer.insertAdjacentHTML('beforeend', modal);
+const createModal = () => {
+  const model = document.getElementById('modal1');
+  const text = document.querySelector('h4');
+  const li = document.querySelector('#card-container');
+  li.addEventListener('click', event => {
+    const del = document.getElementById('allChecklist-container');
+    del.innerHTML = '';
+    const id = event.target.parentNode.getAttribute('data-target');
+    const val = event.target.firstChild.textContent;
+    model.setAttribute('id', id);
+    text.textContent = val;
+
+    getchecklist(id);
+  });
   return;
+};
+
+const createCheckList = (name, id) => {
+  const checkListsContainer = document.getElementById('allChecklist-container');
+  const addNewCheckList = `<div data-id=${id} class="oneChecklist">
+  <div class="checlist-header">
+           <div>
+           <div class="checkListName">
+            <i class="fa fa-check-square-o checklist-icon" aria-hidden="true"></i>
+            <p class="head">${name}</p>
+           </div>
+          <div>
+             <div>
+                <button
+                  class="btn waves-effect waves-light checklistItems"
+                  type="submit"
+                  name="action"
+                  data-type="checkitems">
+                  Add item
+                </button>
+             </div>
+            <div class="addchecklistItems" data-type="additem"> 
+             <div class="card small">
+               <input
+                 type="text"
+                 placeholder="checklist Items"
+                 id="checkListInputItem"
+               />
+               <button class="waves-effect waves-light btn" id="newChecklist">Add
+                 <i class="fa fa-plus-circle" aria-hidden="true"></i>
+               </button>
+               <div class="cancel-icon">
+                 <i class="small material-icons closeChecklistItems" data-type="close_checkitems">cancel</i>
+               </div>
+              </div>
+            </div>
+          </div>
+        </div>
+           <div data-type="delete" class="del">
+            <button class="waves-effect waves-light btn">Delete
+            </button>
+           </div>
+          </div>
+</div>`;
+  checkListsContainer.insertAdjacentHTML('beforeend', addNewCheckList);
+  return;
+};
+
+const getchecklist = async id => {
+  const url = ` https://api.trello.com/1/cards/${id}/checklists?checkItems=all&checkItem_fields=name%2CnameData%2Cpos%2Cstate&key=${apiKey}&token=${token}`;
+  const response = await fetch(url);
+  const JSONresponse = await response.json();
+  JSONresponse.map(ele => {
+    createCheckList(ele.name, ele.id);
+  });
 };
 
 const main = () => {
   getCard();
+  createModal();
   //display add new-card function
   const addbtn = document.querySelector('#btn-newCard');
   const addCard = document.querySelector('#addCard');
@@ -58,12 +115,70 @@ const main = () => {
     addbtn.style.display = 'block';
   });
 
+  //display add new-checklist function
+  const btn_checklist = document.querySelector('#btn-checklist');
+  const addChecklist = document.querySelector('#addChecklist');
+  btn_checklist.addEventListener('click', () => {
+    addChecklist.style.display = 'block';
+    btn_checklist.style.display = 'none';
+  });
+
+  //close new Checklist div container
+  const closeChecklist = document.getElementById('close-tab');
+  closeChecklist.addEventListener('click', () => {
+    addChecklist.style.display = 'none';
+    btn_checklist.style.display = 'block';
+  });
+
+  // create new check list header
+  const checkLists = document.getElementById('newChecklist');
+  checkLists.addEventListener('click', async event => {
+    // console.log(event.target.offsetParent.offsetParent.id);
+    const listName = document.getElementById('checkListInput');
+    if (listName.value === '') {
+      alert('please enter checklist name');
+    } else {
+      const cardId = event.target.offsetParent.offsetParent.id;
+      const url = `https://api.trello.com/1/cards/${cardId}/checklists?name=${listName.value}&key=${apiKey}&token=${token}`;
+      const response = await fetch(url, {
+        method: 'POST'
+      });
+      const checklistData = await response.json();
+      createCheckList(listName.value, checklistData.id);
+      listName.value = '';
+      addChecklist.style.display = 'none';
+      btn_checklist.style.display = 'block';
+    }
+  });
+
+  // remove checklist
+  const deleteChecklist = document.getElementById('allChecklist-container');
+  deleteChecklist.addEventListener('click', async event => {
+    if (event.target.parentNode.getAttribute('data-type') === 'delete') {
+      const checklistID = event.target.parentNode.parentNode.parentNode.getAttribute(
+        'data-id'
+      );
+      const url = `https://api.trello.com/1/checklists/${checklistID}?key=${apiKey}&token=${token}`;
+      await fetch(url, {
+        method: 'DELETE'
+      });
+      event.target.parentNode.parentNode.parentNode.remove();
+    } else if (event.target.getAttribute('data-type') === 'checkitems') {
+      event.target.parentNode.nextElementSibling.style.display = 'block';
+      event.target.style.display = 'none';
+    } else if (event.target.getAttribute('data-type') === 'close_checkitems') {
+      event.target.parentNode.parentNode.parentNode.style.display = 'none';
+      event.target.parentNode.parentNode.parentNode.previousElementSibling.firstElementChild.style.display =
+        'block';
+    }
+  });
+
   //create new card into list
   const newCard = document.getElementById('newCard');
   newCard.addEventListener('click', async () => {
     const text = document.querySelector('input');
     if (text.value === '') {
-      alert('please insert card name');
+      alert('please enter card name');
     } else {
       const url = `https://api.trello.com/1/cards?name=${text.value}&idList=${listId}&keepFromSource=all&key=${apiKey}&token=${token}`;
       const response = await fetch(url, {
@@ -79,7 +194,7 @@ const main = () => {
   //remove card from list
   const cardContainer = document.querySelector('#card-container');
   cardContainer.addEventListener('click', async event => {
-    if (event.target.classList[2] === 'close-card') {
+    if (event.target.getAttribute('data-type') === 'deleteCard') {
       const cardID = event.target.parentNode.parentNode.getAttribute('data-id');
       const url = `https://api.trello.com/1/cards/${cardID}?key=${apiKey}&token=${token}`;
       await fetch(url, {
@@ -88,12 +203,10 @@ const main = () => {
       event.target.parentNode.parentNode.remove();
       event.stopPropagation();
       // return;
-    } else if (event.target.classList[2] === 'cards') {
+    } else {
       const elems = document.querySelectorAll('.modal');
       M.Modal.init(elems);
-      // event.stopPropagation();
     }
-    console.log(event.target);
   });
 };
 
